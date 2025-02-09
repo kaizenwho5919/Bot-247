ef7f469a90930a66d228ee401839b34a6670ea29
kaizenwho5919 <pathakarjun438@gmail.com>
2025-02-09 13:53:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Type Shii", "Created By Kaizenn"];
+const statusMessages = ["Type Shii", "24/7..!?"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
